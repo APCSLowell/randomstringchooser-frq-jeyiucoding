c50cf08fadab631fc3f7495228ec1e5d7b01aba8
jeyiucoding <jeyiu@s.sfusd.edu>
2025-04-26 10:04:38 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,6 +1,24 @@
 import java.util.*;
 public class RandomStringChooser
 {
+  private String[] s;
+  private int v;
+  public RandomStringChooser(String[]x){
+    s = new String[x.length()];
+    for(int i = 0; i < s.length(); i++){
+      s[i] = x[i];
+    }
+    v = x.length();
+  }
+  public getNext(){
+    if(v == 0){
+      return "NONE";
+    }
+    int i = (int)(Math.random()*v);
+    String se = s[i];
+    v-=1;
+    return se;
+  }
   /* to be implemented in part (a) */
   
   //Heads up! 
